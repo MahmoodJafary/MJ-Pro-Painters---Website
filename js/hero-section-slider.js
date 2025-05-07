ad2059be0ee3b6855427d53f7271a7ca695894e7
Mahmood <mahmoodm28@yahoo.com>
2025-05-07 21:45:35 +1000

    remove prev next arrows from slider of hero section

--- js/hero-section-slider.js
+++ js/hero-section-slider.js
@@ -3,10 +3,6 @@ var swiper = new Swiper(".hero-section-slider", {
     effect: "fade",
     lazyLoading: true,
     loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
